cbc72bbaf479ed309be074e5e9cfd72cba2887e9
therealsohail <sohailsaleem1998@gmil.com>
2020-12-20 15:05:13 +0500

    api integeration

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8000;
 const cors = require("cors");
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.options("*", cors());
+
 const stripe = require("stripe")(
   "sk_test_51HzeuECBsWlpmphN7a1VBw1UmVZNIR67udobHiTs7zmYgQ5BHWaHjpgCc4mZTLz8eNwKDckrsV61mGT9jI06MdqP006dNcDTDS"
 );
 
-// const stripe = new Stripe(
-//   "sk_test_51HzeuECBsWlpmphN7a1VBw1UmVZNIR67udobHiTs7zmYgQ5BHWaHjpgCc4mZTLz8eNwKDckrsV61mGT9jI06MdqP006dNcDTDS"
-// );
 app.use(cors());
 app.use(express.json());
 
 app.post("/api/pay", async (req, res) => {
+  console.log(req.body);
   const { id, amount } = req.body;
   try {
     const payment = await stripe.paymentIntents.create({
@@ -32,4 +40,4 @@ app.post("/api/pay", async (req, res) => {
   }
 });
 
-app.listen(8000, () => console.log("Server running"));
+app.listen(port, () => console.log("Server running"));
